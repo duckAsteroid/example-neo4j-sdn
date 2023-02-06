e7c3e105807f6f46afe3e91607e40d9ead8e2487
Darren Smith <darren.smith@cognitran.com>
2023-02-06 12:39:42 +0000

    Change to implement Neo4JRepository

--- src/main/java/com/asteroid/duck/sdn/repo/ClassRepository.java
+++ src/main/java/com/asteroid/duck/sdn/repo/ClassRepository.java
@@ -1,10 +1,9 @@
 package com.asteroid.duck.sdn.repo;
 
 import com.asteroid.duck.sdn.model.Class;
-import com.asteroid.duck.sdn.model.Person;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ClassRepository extends PagingAndSortingRepository<Class, Long> {
+public interface ClassRepository extends Neo4jRepository<Class, Long> {
 }
--- src/main/java/com/asteroid/duck/sdn/repo/PersonRepository.java
+++ src/main/java/com/asteroid/duck/sdn/repo/PersonRepository.java
@@ -1,9 +1,9 @@
 package com.asteroid.duck.sdn.repo;
 
 import com.asteroid.duck.sdn.model.Person;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+public interface PersonRepository extends Neo4jRepository<Person, Long> {
 }
